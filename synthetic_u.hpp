40b3735e7208d54ef42bab84fe308ac8946b9bb6
Gaoyuan Zhang <gyzhang.sjtu@hotmail.com>
2017-08-25 09:57:21 -0400

    init commit

---
+++ synthetic_u.hpp
@@ -0,0 +1,14 @@
+//
+//  synthetic_u.hpp
+//  
+//
+//  Created by Gaoyuan Zhang on 7/26/17.
+//
+//
+
+#ifndef synthetic_u_hpp
+#define synthetic_u_hpp
+
+#include <stdio.h>
+
+#endif /* synthetic_u_hpp */
